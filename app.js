2be6b2f1fd9dd6c9c896c22087442a29ebdc13b5
Rusaid MRD <rusaidmrd@gmail.com>
2019-10-07 20:32:20 +0300

    Testing app.js for 502 bad gateway error

--- app.js
+++ app.js
@@ -29,50 +29,54 @@ app.get('/', (req, res) => {
 });
 
 app.post('/send', (req, res) => {
-    const output = `
-        <p>You have a new contact request</p>
-        <h3>Contact details</h3>
-        <ul>
-            <li>Name : ${req.body.name}</li>
-            <li>Email : ${req.body.email}</li>
-        </ul>
-        <h3>Message</h3>
-        <p>${req.body.message}</p>
-    `;
+    // const output = `
+    //     <p>You have a new contact request</p>
+    //     <h3>Contact details</h3>
+    //     <ul>
+    //         <li>Name : ${req.body.name}</li>
+    //         <li>Email : ${req.body.email}</li>
+    //     </ul>
+    //     <h3>Message</h3>
+    //     <p>${req.body.message}</p>
+    // `;
+
+    console.log('Name is : ' + req.body.name);
+    console.log('Email is : ' + req.body.email);
+    console.log('Message is : ' + req.body.message);
 
     // create reusable transporter object using the default SMTP transport
-    let transporter = nodemailer.createTransport({
-        host: 'rusaidmrd.com',
-        port: 465,
-        secure: true, // true for 465, false for other ports
-        auth: {
-            user: 'me@rusaidmrd.com', // generated ethereal user
-            pass: 'k4J6Z18hhi286998' // generated ethereal password
-        },
-        tls: {
-            // do not fail on invalid certs
-            rejectUnauthorized: false
-        }
-    });
+    // let transporter = nodemailer.createTransport({
+    //     host: 'rusaidmrd.com',
+    //     port: 465,
+    //     secure: true, // true for 465, false for other ports
+    //     auth: {
+    //         user: 'me@rusaidmrd.com', // generated ethereal user
+    //         pass: 'k4J6Z18hhi286998' // generated ethereal password
+    //     },
+    //     tls: {
+    //         // do not fail on invalid certs
+    //         rejectUnauthorized: false
+    //     }
+    // });
 
     // send mail with defined transport object
-    let mailOptions = {
-        from: '"rusaidmrd.com contact 👻" <me@rusaidmrd.com>', // sender address
-        to: 'rusaidpro@gmail.com', // list of receivers
-        subject: 'Email from rusaidmrd.com ✔', // Subject line
-        text: 'Hello world?', // plain text body
-        html: output // html body
-    };
+    // let mailOptions = {
+    //     from: '"rusaidmrd.com contact 👻" <me@rusaidmrd.com>', // sender address
+    //     to: 'rusaidpro@gmail.com', // list of receivers
+    //     subject: 'Email from rusaidmrd.com ✔', // Subject line
+    //     text: 'Hello world?', // plain text body
+    //     html: output // html body
+    // };
 
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            console.log(error)
-        }
-        console.log('Message sent: %s', info.messageId);
-        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-        res.render('index', { msg: 'Email has been sent' });
+    // transporter.sendMail(mailOptions, (error, info) => {
+    //     if (error) {
+    //         console.log(error)
+    //     }
+    //     console.log('Message sent: %s', info.messageId);
+    //     console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+    //     res.render('index', { msg: 'Email has been sent' });
 
-    });
+    // });
 
 
 
