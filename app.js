e3230418800c96676713b4478d5f444c26946c8a
Rusaid MRD <rusaidmrd@gmail.com>
2019-10-08 05:56:31 +0300

    test using gmt emai

--- app.js
+++ app.js
@@ -46,11 +46,11 @@ app.post('/send', (req, res) => {
 
     // create reusable transporter object using the default SMTP transport
     let transporter = nodemailer.createTransport({
-        host: 'mail.rusaidmrd.com',
+        host: 'mail.gmtranslationdoha.com',
         port: 465,
         secure: true, // true for 465, false for other ports
         auth: {
-            user: 'me@rusaidmrd.com', // generated ethereal user
+            user: 'info@gmtranslationdoha.com', // generated ethereal user
             pass: 'k4J6Z18hhi286998' // generated ethereal password
         },
         tls: {
@@ -61,7 +61,7 @@ app.post('/send', (req, res) => {
 
     // send mail with defined transport object
     let mailOptions = {
-        from: '"rusaidmrd.com contact 👻" <me@rusaidmrd.com>', // sender address
+        from: '"gmtranslationdoha.com contact 👻" <info@gmtranslationdoha.com>', // sender address
         to: 'rusaidpro@gmail.com', // list of receivers
         subject: 'Email from rusaidmrd.com ✔', // Subject line
         text: 'Hello world?', // plain text body
